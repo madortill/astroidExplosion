cfea65dec0f22c06d31d04818da6b4b2554769b8
madorTill <mador.till@gmail.com>
2025-03-31 18:29:22 +0300

    add changes

--- src/components/StartExplanation.vue
+++ src/components/StartExplanation.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="start-explanation">
         <div class="bubble"> 
-            <p>{{ text }}</p>
+            <p class="text">{{ text[page] }}</p>
+            <img @click="page++" v-if="page==0" class="arrow-left" src="/src/assets/media/app/arrow.svg" alt="arrow-left">
+            <button class="button">בואו נצא למסע!</button>
         </div>
         <img class="yoda" src="/src/assets/media/app/yoda.svg" alt="yoda">
     </div>
@@ -14,7 +16,11 @@ export default {
     },
     data() {
         return {
-
+            text: [
+            `היי אני יודה!  אתם יכולים להודות לי שהצלתי אתכם מהאסטרואיד, תודה רבה!`,
+            "שמעתי שגם אתם רוצים ללמוד לפוצץ אסטרואידים. הצטרפו אליי למסע ובסופו תהפכו לג’דיי אמיתי!"
+            ],
+            page: 0,
         }
     },
     methods: {
@@ -31,15 +37,38 @@ export default {
         background-image: url(/src/assets/media/opening/speech-bubble-open.svg);
         background-size: 100% 100%;
         right: 50%;
-        top: 1rem;
+        top: 7rem;
         transform: translateX(50%);
     }
 
     .yoda {
-        height: 18rem;
+        height: 20rem;
         position: absolute;
         right: 50%;
         bottom: 1rem;
         transform: translateX(50%);
+    }
+
+    .arrow-left {
+        position: absolute;
+        left: 5rem;
+        bottom: 7rem;
+        rotate: 180deg;
+    }
+
+    .text {
+        position: absolute;
+        left: 50%;
+        transform: translateX(-50%);
+        text-align: center;
+        width: 20rem;
+        font-size: 1.8rem;
+    }
+
+    .button {
+        background-color: #FEC91A;
+        font-family: "secolar";
+        border: none;
+        border-radius: 0.1rem;
     }
 </style>
